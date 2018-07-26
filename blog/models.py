52ca30b59b7eec3d251f796dae13360a1769c41c
Esmaeel Komijani <EsmaeelKomijani@gmail.com>
2018-07-26 20:32:48 +0430

    finished project

--- blog/models.py
+++ blog/models.py
@@ -8,4 +8,17 @@ class Blog(models.Model):
     body = models.TextField(max_length=2000)
     image = models.ImageField(upload_to='images/')
 
+    def __str__(self):
+        return self.title
+
+
+    def summary(self):
+        return self.body[:150]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    
+
+
     
